cd543e14bfacbd4981dd42275783eda909b2fcef
S-samira2020 <54632872+S-samira2020@users.noreply.github.com>
2020-08-22 03:33:56 +0600

    Add files via upload

---
+++ program7.py
@@ -0,0 +1,22 @@
+# sort list of cars in alphabetically
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars)
+print()
+# sort list in alphabetically in the reverse way
+cars.sort(reverse = True)
+print(cars)
+print()
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print('here is the actual lis:\n', cars)
+print()
+print('here is the sorted list\n', sorted(cars))
+print()
+print('here is the original list again\n', cars)
+print('here is the reverse list')
+cars.reverse()
+print(cars)
+print()
+cars.reverse()
+print(cars)
+print()
