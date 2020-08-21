734c640dcee12c927026c2221e69fd3dd1508686
S-samira2020 <54632872+S-samira2020@users.noreply.github.com>
2020-08-22 02:28:58 +0600

    Add files via upload

---
+++ program6.py
@@ -0,0 +1,43 @@
+'''messages = ['sakiba', 'sagor', 'salma']
+print(messages)
+p_messages  = messages.pop()
+print(messages)
+messages.append('Sharif')
+print(messages)
+messages2 = ['sagor', 'samira', 'kakku']
+print(messages2)
+messages.insert(0, "Samira")
+print(messages)
+messages.insert(2, 'rafi')
+print(messages)
+f = messages.pop(1)
+print('i can invite wo peaple for dinner' + f.title() + '.')
+print()
+'''
+
+names = ['jamil', 'kamil', 'mamil', 'papil', 'kapil', 'sunil', 'shushil']
+print(names)
+print()
+print('I can invite only two peaople for dinner')
+name = names.pop()
+print(names)
+name = names.pop()
+print(names)
+name = names.pop()
+print(names)
+name = names.pop()
+print(names)
+name = names.pop()
+print(names)
+names = ['jamil', 'kamil']
+print('they are invited')
+print(names)
+del names[0]
+print(names)
+names = ['jamil']
+del names[0]
+names
+print('empty list')
+names
+
+
